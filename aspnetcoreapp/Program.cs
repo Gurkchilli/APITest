78a13f8e1cb3ea8f098a1143f3eeeb4c16bfde2e
Erik Hildinge <erik.hildinge@gmail.com>
2020-06-05 23:35:43 +0200

    add the functionality of pressing enter instead of pressing the button

--- aspnetcoreapp/Program.cs
+++ aspnetcoreapp/Program.cs
@@ -11,21 +11,77 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
+using System.Globalization;
+using System.Text.Json.Serialization;
+
 namespace aspnetcoreapp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task Main(string[] args)
         {
+            //Fetches the different variables
+            /*
+            var repositories = await ProcessRepositories();
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine();
+            }
+            */
+
             CreateHostBuilder(args).Build().Run();
+
+            
         }
 
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            return repositories;
+        }
+
+        //Creates the 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [JsonPropertyName("homepage")]
+        public Uri Homepage { get; set; }
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public string JsonDate { get; set; }
+
+        public DateTime LastPush =>
+            DateTime.ParseExact(JsonDate, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
     }
 }
