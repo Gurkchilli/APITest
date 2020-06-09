f9f737e3e46bf1f8781bf84791e00c40db47a08b
Erik Hildinge <erik.hildinge@gmail.com>
2020-06-09 17:41:03 +0200

    Clean up the localhost site

--- WebAPI/Controllers/ArtistController.cs
+++ WebAPI/Controllers/ArtistController.cs
@@ -16,7 +16,8 @@ namespace WebAPI.Controllers
         // GET api/artist
         public string Get()
         {
-            return  "Finish the link with a \"/mbid\"<br>Some examples are:<br>" +
+            return  "Finish the link with a <q>/mbid</q><br>" +
+                    "Some examples of mbid's are:<br>" +
                     "Nirvana : 5b11f4ce-a62d-471e-81fc-a69a8278c7da<br>" +
                     "Linking Park : f59c5520-5f46-4d2c-b2c4-822eabf53419<br>" +
                     "Eminem : b95ce3ff-3d05-4e87-9e01-c97b66af13d4<br>" +
@@ -26,8 +27,8 @@ namespace WebAPI.Controllers
         // GET api/artist/5
         public async Task<JObject> Get(string id)
         {
-            var hej = await Program.ReturnJson(id);
-            return hej;
+            var jObject = await Program.ReturnJson(id);
+            return jObject;
         }
     }
 }
